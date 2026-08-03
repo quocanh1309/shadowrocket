db108fc8fe9073efd306e9b8949619719b63d206
quocanh1309 <84062728+quocanh1309@users.noreply.github.com>
2026-08-03 10:23:24 +0700

    Update Locket_Gold.js

--- Locket_Gold.js
+++ Locket_Gold.js
@@ -1,11 +1,14 @@
+// ========= ID ========= //
 const mapping = {
   '%E8%BD%A6%E7%A5%A8%E7%A5%A8': ['vip+watch_vip'],
   'Locket': ['Gold']
 };
+// =========   Phần cố định  ========= // 
+// =========  @duyvinh09 ========= // 
 var ua = $request.headers["User-Agent"] || $request.headers["user-agent"],
   obj = JSON.parse($response.body);
 obj.Attention = "Chúc mừng bạn! Vui lòng không bán hoặc chia sẻ cho người khác!";
-var hin = {
+var duyvinh09 = {
       auto_resume_date: null,
       display_name: "locket_1600_1y",
       is_sandbox: true,
@@ -17,26 +20,26 @@ var hin = {
           "amount": 399000.0,
           "currency": "VND"
       },
-      expires_date: "9999-14-04T10:10:14Z",
+      expires_date: "9999-01-09T10:10:14Z",
       grace_period_expires_date: null,
       refunded_at: null,
       unsubscribe_detected_at: null,
-      original_purchase_date: "2008-14-04T10:10:15Z",
-      purchase_date: "2008-14-04T10:10:14Z",
+      original_purchase_date: "2005-01-09T10:10:15Z",
+      purchase_date: "2005-01-09T10:10:14Z",
       store: "app_store",
       store_transaction_id: "2000001108724193",
   },
   locketGold = {
       grace_period_expires_date: null,
-      purchase_date: "2008-14-04T10:10:14Z",
+      purchase_date: "2005-01-09T10:10:14Z",
       product_identifier: "locket_1600_1y",
-      expires_date: "9999-14-04T10:10:14Z"
+      expires_date: "9999-01-09T10:10:14Z"
   };
 const match = Object.keys(mapping).find(e => ua.includes(e));
 if (match) {
   let [e, s] = mapping[match];
-  s ? (locketGold.product_identifier = s, obj.subscriber.subscriptions[s] = hin) : obj.subscriber.subscriptions["locket_1600_1y"] = hin, obj.subscriber.entitlements[e] = locketGold
-} else obj.subscriber.subscriptions["locket_1600_1y"] = hin, obj.subscriber.entitlements.pro = locketGold;
+  s ? (locketGold.product_identifier = s, obj.subscriber.subscriptions[s] = duyvinh09) : obj.subscriber.subscriptions["locket_1600_1y"] = duyvinh09, obj.subscriber.entitlements[e] = locketGold
+} else obj.subscriber.subscriptions["locket_1600_1y"] = duyvinh09, obj.subscriber.entitlements.pro = locketGold;
 $done({
   body: JSON.stringify(obj)
 });
